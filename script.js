3f8333f056c217d55ed8121f29f61ebdd5541a1c
John Malcolm Anderson <jmanderson@verizonmedia.com>
2020-03-31 20:49:04 +0100

    Added to-do list and fixed north-inner-city

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(e) {
     console.log("Council Mapper");
 
+    // 0. Setup SVG with d3.geoAlbers projection type
+    // 1. Make AJAX Requests for 11 LEA - GeoJSON files from S3 bucket and store in array
+    // 2. Loop through the array, for each element create a merged topology object and append to SVG
+    // 3. Add MapBox raster to d3 SVG map.
+    // 4. Add zoom functionality
+    // 5. Show sub LEA on zoom, and remove when zoomed out.
+    // 6. Add hover affects showing LEA info.
+    // 7. Add toggle various cloropleths. 
+
     d3.json("server/dublin-city-lea.json", function(error, electoral) {
         if (error) return console.error("Error: " + error)
         
@@ -14,10 +23,10 @@ document.addEventListener("DOMContentLoaded", function(e) {
         var width = 850,
         height = 700;
 
-        var topology = topojson.topology({electoral});
-        console.log(topology);
+        // var topology = topojson.topology({electoral});
+        // console.log(topology);
         
-        var topoMerged = topojson.merge(topology, topology.objects.electoral.geometries);
+        // var topoMerged = topojson.merge(topology, topology.objects.electoral.geometries);
 
         // Create SVG variable & append to DOM
         var svg = d3.select("body").append("svg")
@@ -43,10 +52,10 @@ document.addEventListener("DOMContentLoaded", function(e) {
                     " " + d.properties.EDNAME.replace(/\W+/g, '-').toLowerCase() })
               .attr("d", path);
 
-        svg.append("path")
-            .datum(topoMerged)
-            .attr("class", "state selected")
-            .attr("d", path);
+        // svg.append("path")
+        //     .datum(topoMerged)
+        //     .attr("class", "state selected")
+        //     .attr("d", path);
 
     });
 });
